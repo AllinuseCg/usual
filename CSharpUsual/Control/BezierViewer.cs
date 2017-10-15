4579132d80959707b9313706df6d1cbb09c43cc0
allinuse <1163387982@qq.com>
2017-10-15 17:41:16 +0800

    新建项目开始写bezier类和测试用的控件

---
+++ CSharpUsual/Control/BezierViewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpUsual.Control
+{
+    class BezierViewer : System.Windows.Forms.UserControl
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BezierViewer
+            // 
+            this.Name = "BezierViewer";
+            this.Size = new System.Drawing.Size(255, 255);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
